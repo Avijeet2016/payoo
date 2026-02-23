1c7cb0e71d50ff29c562010893ab7a1c09b9cd07
Avijeet <avijeet.cse1.bu@gmail.com>
2026-02-24 00:02:26 +0600

    homepage structure is ready with cashout feature

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,16 @@
-console.log("login.js is working now");
+document.getElementById('login-btn').addEventListener('click', function() {
+    const inputMobile = document.getElementById('input-mobile');
+    const contactNumber = inputMobile.value;
+    
+    const inputPin = document.getElementById('input-pin');
+    const pin = inputPin.value;
+    
+    if (contactNumber == "01234567890" && pin == "1234") {
+        alert('Login successful');
+        window.location.assign('/home.html');
+    }
+    else {
+        alert('Login Failed');
+        return;
+    }
+});
